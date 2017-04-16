b0fda5a9679897dc8ee65f110a652771bbd99b3c
Yuta Iritani <tayutayura@gmail.com>
2017-04-16 13:21:56 +0000

    asdf setup

---
+++ init/asdf.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+. ./get_os.sh
+
+os=$(get_os)
+
+if [ ${os} === 'Mac' ]; then
+  brew install automake autoconf openssl libyaml readline libxslt libtool unixodbc
+else
+  sudo yum -y install automake autoconf readline-devel ncurses-devel openssl-devel libyaml-devel libxslt-devel libffi-devel libtool unixODBC-devel
+fi
+
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.2.1
+
+echo -e '\n# SETUP asdf.' >> ~/.bashrc
+
+if [ ${os} === 'Mac' ]; then
+  # OR for Mac OSX
+  echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bash_profile
+  echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bash_profile
+else
+  # For Ubuntu or other linux distros
+  echo -e '. $HOME/.asdf/asdf.sh' >> ~/.bashrc
+  echo -e '. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
+fi
+
+exec $SHELL -l
