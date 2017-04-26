dc3e1d712ca6b386a375174ff41a72b9cde755f3
Yuta Iritani <tayutayura@gmail.com>
2017-04-26 08:57:27 +0000

    install php-fpm

--- init/php-nginx.sh
+++ init/php-nginx.sh
@@ -8,11 +8,10 @@ sudo cp -p /etc/php.ini /etc/php.ini.org
 sudo sed -i -e 's/;date.timezone =/date.timezone = Asia/Tokyo/' /etc/php.ini
 
 sudo cp -p /etc/php-fpm.d/www.conf /etc/php-fpm.d/www.conf.org
-sudo sed -i -e 's/user = apache/user = nginx/' /etc/php-fpm.d/www.conf
-sudo sed -i -e 's/group = apache/group = nginx/' /etc/php-fpm.d/www.conf
-sudo sed -i -e 's/listen = 127.0.0.1:9000/listen = \/var\/run\/php-fpm\/php-fpm.sock/' /etc/php-fpm.d/www.conf
-sudo sed -i -e 's/;listen.owner = nobody/listen.owner = nginx/' /etc/php-fpm.d/www.conf
-sudo sed -i -e 's/;listen.group = nobody/listen.group = nginx/' /etc/php-fpm.d/www.conf
+sudo sed -i -e 's/user = apache/user = ec2-user/' /etc/php-fpm.d/www.conf
+sudo sed -i -e 's/group = apache/group = ec2-user/' /etc/php-fpm.d/www.conf
+sudo sed -i -e 's/;listen.owner = nobody/listen.owner = ec2-user/' /etc/php-fpm.d/www.conf
+sudo sed -i -e 's/;listen.group = nobody/listen.group = ec2-user/' /etc/php-fpm.d/www.conf
 
 sudo /etc/rc.d/init.d/php-fpm start
 sudo chkconfig php-fpm on
