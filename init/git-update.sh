527d193bdd5d4911a2fade61ee1939c8547ba9c5
Yuta Iritani <tayutayura@gmail.com>
2017-05-06 17:01:13 +0000

    clean

--- init/git-update.sh
+++ init/git-update.sh
@@ -2,6 +2,7 @@
 
 cd /usr/local/src
 sudo git clone git://git.kernel.org/pub/scm/git/git.git
+
 cd git
 sudo make prefix=/usr/local all
 sudo make prefix=/usr/local install
--- init/git.sh
+++ init/git.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-sudo yum -y install gcc gcc-c++ curl-devel expat-devel gettext-devel openssl-devel zlib-devel perl-ExtUtils-MakeMaker
+sudo yum -y install gcc\
+                    gcc-c++\
+                    curl-devel\
+                    expat-devel\
+                    gettext-devel\
+                    openssl-devel\
+                    zlib-devel\
+                    perl-ExtUtils-MakeMaker
+
 cd /usr/local/src
 sudo wget https://www.kernel.org/pub/software/scm/git/git-2.11.0.tar.gz
 sudo tar zxvf git-2.11.0.tar.gz
+
 cd git-2.11.0
 sudo make prefix=/usr/local all
 sudo make prefix=/usr/local install
