797e055a9e5cd54b5527620f7bb03f657ad148bc
tayutayura <tayutayura@gmail.com>
2018-01-23 13:53:22 +0000

    add messages

--- initializers/asdf.sh
+++ initializers/asdf.sh
@@ -1,3 +1,4 @@
+echo -e "\033[0;32m[asdf] Install dependencies\033[0;39m"
 sudo yum install -y automake\
                     autoconf\
                     readline-devel\
@@ -9,11 +10,25 @@ sudo yum install -y automake\
                     libtool\
                     unixODBC-devel
 
+echo -e "\033[0;32m[asdf] Clone asdf from Github\033[0;39m"
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.4.1
 
-# For Ubuntu or other linux distros
-echo -e '\n# SETUP asdf.' >> ~/.bashrc
-echo -e '. $HOME/.asdf/asdf.sh' >> ~/.bashrc
-echo -e '. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
+# write initial setting for .vimrc
+{
+    echo ""
+    echo "Please add the following settings to your .bashrc file:"
+    echo "--------------------------------------------------"
+    echo ""
+    echo "# SETUP asdf."
+    echo ". $HOME/.asdf/asdf.sh"
+    echo ". $HOME/.asdf/completions/asdf.bash"
+    echo ""
+    echo "--------------------------------------------------"
+}
+echo -e "\033[0;32m[asdf] Complete!\033[0;39m\n"
 
-exec $SHELL -l
+# exec $SHELL -l
+
+for file in ${INIT_DIR}/asdf.d/*.sh; do
+  source ${file}
+done
