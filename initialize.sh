03c4deea9ea31c8b40438cd50d1abcf4abbdcc7e
tayutayura <tayutayura@gmail.com>
2018-01-23 16:48:17 +0000

    共通処理は ./initializers/common/ 以下に記述する

--- initialize.sh
+++ initialize.sh
@@ -8,7 +8,12 @@ else
   INIT_DIR="./initializers/linux"
 fi
 
+COMMON_DIR="./initializers/common"
+
 for file in ${INIT_DIR}/*.sh; do
-#echo ${file}
+  source ${file}
+done
+
+for file in ${COMMON_DIR}/*.sh; do
   source ${file}
 done
