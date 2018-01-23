e25c2d615710c6d83563ffcb1fc430a6fad68f17
tayutayura <tayutayura@gmail.com>
2018-01-23 13:50:45 +0000

    delete copied files

--- init/nginx.sh
+++
@@ -1,6 +0,0 @@
-sudo rpm -ivh http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm
-sudo yum -y install nginx
-
-sudo /etc/rc.d/init.d/nginx configtest
-sudo /etc/rc.d/init.d/nginx start
-sudo chkconfig nginx on
