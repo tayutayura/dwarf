d0f884865ec54ceb7b630d371178057b1f519da5
tayutayura <tayutayura@gmail.com>
2018-02-20 16:42:05 +0000

    install ctags when initialize

--- initializers/linux/1_utils.sh
+++ initializers/linux/1_utils.sh
@@ -5,4 +5,5 @@ sudo yum -y update
 # Install utility tools
 sudo yum install -y wget\
                     unzip\
-                    yum-utils
+                    yum-utils\
+                    ctags
