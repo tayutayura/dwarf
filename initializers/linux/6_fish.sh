1bdc352d894480a8abe8ca7e6c1d0b2798d66eb8
tayutayura <tayutayura@gmail.com>
2018-02-25 13:33:43 +0000

    add fish shell

---
+++ initializers/linux/6_fish.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install Fish
+# cd /etc/yum.repos.d/
+# sudo wget https://download.opensuse.org/repositories/shells:fish:release:2/CentOS_7/shells:fish:release:2.repo
+# sudo yum -y install fish
+sudo chsh -s /usr/bin/fish `whoami`
+
