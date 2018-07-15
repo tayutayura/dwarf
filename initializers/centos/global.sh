3e97e581d99de6176ed656b586c452e15bc2f0d9
tayutayura <tayutayura@gmail.com>
2018-07-16 05:53:05 +0900

    mv global to common dir

--- initializers/centos/global.sh
+++ initializers/centos/global.sh
@@ -1,36 +1,3 @@
 #!/bin/bash
 
-GLOBAL_VERSION=6.6.2
-
-global__install_pygments()
-{
-  if type pip3; then
-    pip3 install pygments
-    return 0
-  else
-    return 1
-  fi
-}
-
-global__install()
-{
-  if type global; then
-    (
-      cd ${HOME}
-      wget http://tamacom.com/global/global-${GLOBAL_VERSION}.tar.gz
-      tar zxvf global-${GLOBAL_VERSION}.tar.gz
-      cd global-${GLOBAL_VERSION}
-      PYTHON=${HOME}/.asdf/shims/python ./configure
-      sudo make
-      sudo make install
-      cd ${HOME}
-      rm -f global-${GLOBAL_VERSION}.tar.gz
-      rm -rf global-${GLOBAL_VERSION}
-    )
-  fi
-}
-
-global__install_pygments
-if [ $? -ne 1 ]; then
-  global__install
-fi
+. ${DWARF_INITIALIZERS_COMMON_DIR}/global.sh
