a8b6d21049f1291c4187e003383d0152c1aedaa6
tayutayura <tayutayura@gmail.com>
2018-07-16 04:24:17 +0900

    Update install utils script

--- initializers/osx/utils.sh
+++ initializers/osx/utils.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+declare -a INSTALL_LIST=(wget jq)
+
 # Install utility tools
 utils__install()
 {
-  brew install wget jq
+  local -a install_list=${INSTALL_LIST[@]}
+  while read -r formura
+  do
+    local -i i=0
+    for f in ${install_list[@]}; do
+      if [ "${formura}" == $f ]; then
+        unset install_list[$i]
+      fi
+      let i++
+    done
+  done < <(brew list 2> /dev/null)
+
+  if [ ${#array[@]} -ne 0 ]; then
+    brew install ${install_list}
+  else
+    info "utilis are already installed."
+  fi
 }
 
 utils__install
